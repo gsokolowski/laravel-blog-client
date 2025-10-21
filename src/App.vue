6a0548d787fc70b650aa56c0d01fcee75aa986ba
Greg <greg@mac.home>
2025-10-21 02:41:10 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="text-3xl font-bold underline text-red-600">
+    Hello world! axios
+  </h1>
+</template>
+
+<script setup>
+import axios from 'axios';
+axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie').then(response => {
+    // Login...
+});
+</script>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import axios from 'axios' // import axios
+
+// axios canfiguration for fetching data from laravel-blog-api
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
